d8899d402f52b1f861736ef7c243e7ae4ab7d700
zsmcdonald <stephen.mcdonald@aquaq.co.uk>
2018-07-09 13:23:57 +0100

    Improve start code generation

--- bash/csvoutput.sh
+++ bash/csvoutput.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-tail -n +2 process.csv | awk -F "\"*,\"*" '{print "The baseport is " $2 " which does something amazing.\n"}'
+
+####Format should appear as below####
+#check process $procname
+#    matching \"$KDBSTACKID -proctype $proctype -procname $procname\"
+#    start program = \"/bin/bash $startscript $procname\"
+#        with timeout 10 seconds
+#    stop program = \"/bin/bash $stopscript $procname\"
+#    every \"* * * * *\"
+#    mode active
+####-----------------------------####
+
+torqscript=torq.sh
+
+tail -n +2 process.csv | awk -v awktorq=$torqscript -F "\"*,\"*" '{print  "check process "$4"\n    matching \"$KDBSTACKID -proctype "$3 " -procname " $4"\"" "\n    start program = \\\"/bin/bash " awktorq " start " $4 "\n        with timeout 10 seconds\n    stop program = \\\"/bin/bash " awktorq " stop " $4 "\n        with timeout 10 seconds\n    every  \\\"* * * * *\\\"\n    mode active\n"}'
+
+
+#tail -n +2 process.csv | awk -F "\"*,\"*" '{print  "check process "$4"\n    matching \"$KDBSTACKID -proctype "$3 " -procname " $4"\"""\n        with timeout 10 seconds\n    stop program = \\\"/bin/bash" $stopscript}'
+
